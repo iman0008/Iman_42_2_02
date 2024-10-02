9ce25b431f93fe9898d6f6eeb71bacf211540c9d
Qwerty_OoO <alymkuloviman46@gmail.com>
2024-10-02 19:21:28 +0600

    Add files via upload

---
+++ project 1/js/home_works.js
@@ -0,0 +1,26 @@
+let inputGmail = document.querySelector("#gmail_input");
+let buttonGmail = document.querySelector("#gmail_button");
+let resultGmail = document.querySelector("#gmail_result");
+
+const regExpGmail = /@gmail.com/;
+buttonGmail.onclick = () => {
+  if (regExpGmail.test(inputGmail.value)) {
+    resultGmail.innerHTML = "you are registration successful!";
+    resultGmail.style.color = "green";
+  } else {
+    resultGmail.innerHTML = "you not registration";
+    resultGmail.style.color = "red";
+  }
+};
+
+let child = document.querySelector(".child_block");
+let num = 0;
+const counter = () => {
+  num++;
+  child.style.left = `${num}px`;
+  if (num < 448) {
+    requestAnimationFrame(counter);
+  }
+  //   counter
+};
+counter();
